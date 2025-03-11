4b9ca5e587c03c1f05a354b34f34043fa9945732
monster-1951 <shivashankarsince19@gmail.com>
2025-03-11 14:06:21 +0530

    second commit

--- src/app/api/askk/route.ts
+++ src/app/api/askk/route.ts
@@ -1,8 +1,13 @@
 
+
 export async function POST(req: Request) {
+  const { input } = await req.json();
+
+
 
   // Return the extracted response
   return Response.json({
+    input,
     content:"The Bhagavad Gita mentions three main types of Yoga: Karma Yoga, Bhakti Yoga, and Jnana Yoga. Karma Yoga focuses on selfless action and performing duties without attachment to the results. Bhakti Yoga emphasizes devotion and love for a higher power, while Jnana Yoga focuses on knowledge and self-realization through understanding the true nature of the self."
   });
 }
